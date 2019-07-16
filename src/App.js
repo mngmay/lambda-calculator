133cf568ae6d99a8bad0f8c256ec032c02d658ed
May Ng <mngmay@gmail.com>
2019-07-16 13:52:44 -0700

    wip - styling calculator

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
@@ -23,9 +24,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <Specials />
         <Numbers />
         <Operators />
-        <Specials />
       </div>
     </div>
   );
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Numbers from "..ButtonComponents/NumberButtons/Numbers";
-import "./styles.module.css";
 
 const Display = () => {
-  return (
-    <div>
-      {/* Display any props data here */}
-      <Numbers />
-    </div>
-  );
+  return <div>{/* Display any props data here */}</div>;
 };
+
+export default Display;
