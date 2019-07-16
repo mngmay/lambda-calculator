7ac8cf2e05634cbeb84ca9e5400cf480ae205abf
May Ng <mngmay@gmail.com>
2019-07-16 15:43:57 -0700

    wip - calculator 0 button styled. added unique classes in NumberButton creation

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { tsPropertySignature } from "@babel/types";
 
 const NumberButton = ({ number }) => {
   //could also pass props as argument
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className="numberButton">{number}</button>
+      <button className={`numberButton button-${number}`}>{number}</button>
       {/* Can also do:  <button className="numberButton">{props.number}</button>  */}
     </>
   );
